fce50bb56a343516aee1f63d37089c689bd9d9d4
WillianASantos <wil999488109@gmail.com>
2024-09-07 17:49:27 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+
+let dados = [
+    {
+        titulo: "Estética",
+        descricao: "A estética é um ramo da filosofia que se dedica a investigar a natureza da beleza, a arte e a experiência estética. Ao invés de se concentrar em questões práticas como a ética, ou em questões sobre a natureza da realidade como a metafísica, a estética se volta para o mundo dos sentidos e das emoções, buscando compreender o que torna algo belo e como essa beleza nos afeta.",
+        pensadores: "Filósofos relacionados ao tema: Platão, Kant, Hegel, Heidegger",
+        link: "https://pt.wikipedia.org/wiki/Est%C3%A9tica",
+        tags: "estetica, belo, feio, bonito, ruim, prazer"
+    },
+    {
+        titulo: "Política",
+        descricao: "A filosofia política é um ramo da filosofia que se dedica a investigar as bases teóricas e práticas da organização social, do poder, da justiça e da lei. Os filósofos políticos buscam compreender como as sociedades são estruturadas, como o poder é distribuído e exercido, e quais são os princípios que devem guiar a vida em comunidade.",
+        pensadores:"Filósofos relacionados ao tema: Aristóteles, John Locke, Karl Marx, Hannah Arendt",
+        link: "https://pt.wikipedia.org/wiki/Filosofia_pol%C3%ADtica",
+        tags: "politica, sociedade, Estado"
+    },
+    {
+        titulo: "Ética",
+        descricao: "A ética na filosofia é um campo de estudo que se dedica a investigar os fundamentos da ação moral. Em outras palavras, a ética busca compreender o que é o bem, o mal, o justo e o injusto, e quais são os princípios que devem guiar nossas ações.",
+        pensadores: "Filósofos relacionados ao tema: Aristóteles, Kant, Nietzsche, Peter Singer", // Aspas duplas internas corrigidas
+        link: "https://pt.wikipedia.org/wiki/%C3%89tica",
+        tags: "certo, errado, bom, mal, etica, moral"
+    }
+];
